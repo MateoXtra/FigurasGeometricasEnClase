894bc87be6d6173806324958affcf6491d86532c
POO <kevin.mendoza01@epn.edu.ec>
2024-05-31 12:41:11 -0500

    kevinprueba2

--- src/Figuras/Circulos.java
+++ src/Figuras/Circulos.java
@@ -1,4 +1,34 @@
 package Figuras;
 
 public class Circulos {
+    Float radio;
+    Float radio_cuadrado;
+
+    public Circulos(){
+
+    }
+    public Circulos(Float radio, Float radio_cuadrado){
+        this.radio = radio;
+        this.radio_cuadrado = radio_cuadrado;
+    }
+
+    public Float getRadio() {
+        return radio;
+    }
+
+    public Float getRadio_cuadrado() {
+        return radio_cuadrado;
+    }
+
+    public void setRadio(Float radio) {
+        this.radio = radio;
+    }
+
+    public void setRadio_cuadrado(Float radio_cuadrado) {
+        this.radio_cuadrado = radio_cuadrado;
+    }
+    public void mostrar_datos_Circulo(){
+        System.out.println("RADIO DE CIRCULO: " + radio);
+        System.out.println("RADIO DE CIRCULO: " + radio_cuadrado);
+    }
 }

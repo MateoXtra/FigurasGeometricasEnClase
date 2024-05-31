c3ebfcdbe5e334769e41161cb58c1eceab198cff
mateoxtra <mateoextra93@gmail.com>
2024-05-31 12:03:31 -0500

    clase padre

--- src/Figuras/FigurasGeometricas.java
+++ src/Figuras/FigurasGeometricas.java
@@ -3,6 +3,35 @@ package Figuras;
 public class FigurasGeometricas {
     int nLados;
     String Nombre;
+
+    public FigurasGeometricas() {
+    }
+
+    public FigurasGeometricas(int nLados, String nombre) {
+        this.nLados = nLados;
+        Nombre = nombre;
+    }
+
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public void mostrar_datos() {
+        System.out.println("Nombre: " + Nombre);
+        System.out.println("nLados: " + nLados);
+    }
 }
 
 

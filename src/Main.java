3d3b3ec1021b52e6cb2347b171a33623dfc2a91f
mateoxtra <mateoextra93@gmail.com>
2024-05-31 12:29:15 -0500

    Menu a medias :D

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,15 @@ public class Main {
             switch(opcion){
                 case 1:
                     System.out.println("1.Figuras 2D");
-                    System.out.println("1.Figuras 3D");
+                    System.out.println("2.Figuras 3D");
+                    System.out.println("Ingrese opcion: ");
+                    opcion = scanner.nextInt();
+                    if (opcion == 1){
+                        System.out.println("1.Regulares");
+                        System.out.println("2.Irregulares");
+                        System.out.println("Ingrese opcion: ");
+                        opcion = scanner.nextInt();
+                    }
 
             }
         }while (opcion != 2);

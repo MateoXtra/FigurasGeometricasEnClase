a788e2b6eb43e12ba4daad8ead9e44a80cbe3f07
Marl0nico <marlon.nicolalde@epn.edu.ec>
2024-06-04 23:23:07 -0500

    Implementación de métodos en figuras irregulares y correción de clases

--- src/Figuras/Cuadrilatero.java
+++ src/Figuras/Cuadrilatero.java
@@ -1,19 +1,16 @@
 package Figuras;
 
-public class Cuadrilatero extends Figuras2D {
-    private double lado1;
-    private double lado2;
-
+public class Cuadrilatero extends Regulares {
+    double lado1;
+    double lado2;
     public Cuadrilatero(double lado1, double lado2) {
         this.lado1 = lado1;
         this.lado2 = lado2;
     }
-
     @Override
     public double calcularArea() {
         return lado1 * lado2;
     }
-
     @Override
     public double calcularPerimetro() {
         return 2 * (lado1 + lado2);
--- src/Figuras/Triangulo.java
+++ src/Figuras/Triangulo.java
@@ -1,22 +1,19 @@
 package Figuras;
 
-public class Triangulo extends Figuras2D {
-    private double base;
-    private double altura;
-
+public class Triangulo extends Regulares{
+    double base;
+    double altura;
+    double hipotenusa;
     public Triangulo(double base, double altura) {
         this.base = base;
         this.altura = altura;
     }
-
     @Override
     public double calcularArea() {
         return (base * altura) / 2;
     }
-
     @Override
     public double calcularPerimetro() {
-        
-        return -1;
+        return base+altura+hipotenusa;
     }
 }

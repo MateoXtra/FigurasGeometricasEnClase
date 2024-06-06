c08bd57d3cff625646d5ebef6cd4877d1c3cb894
StalinMuela <stalinsangucho87@gmail.com>
2024-06-05 20:08:09 -0500

    Correccion

--- src/Figuras/Figuras3D.java
+++ src/Figuras/Figuras3D.java
@@ -20,6 +20,12 @@ public class Figuras3D extends FigurasGeometricas{
         this.altura = altura;
     }
 
+
+
+    public Double getLado() {
+        return lado;
+    }
+
     public void setLado(Double lado) {
         this.lado = lado;
     }
@@ -40,185 +46,122 @@ public class Figuras3D extends FigurasGeometricas{
         this.altura = altura;
     }
 
-
-    // CALCULOSS ------------------------------------- //
-
     public Double generatriz(){
         Double valor = Math.sqrt(Math.pow(altura,2) + Math.pow(radio,2));
         return valor;
     }
-
-    //CUBO//
-    public Double CalcularAreaCubo(){
-        setnLados(6);
-        Double area = nLados * Math.pow(lado, 2);
-        return area;
-    }
-    public Double CalcularVolumenCubo(){
-        Double volumen = Math.pow(lado, 3);
-        return volumen;
-    }
-
-    //ESFERA//
-    public Double CalcularAreaEsfera(){
-        Double area = 4 * PI * Math.pow(radio,2);
-        return area;
-    }
-    public Double CalcularVolunenEsfera(){
-        Double volumen = 3/4 * PI * Math.pow(radio,3);
-        return volumen;
+    public double calcularArea(String nombre) {
+        switch (nombre) {
+            case "Cubo":
+                return 6 * Math.pow(lado, 2);
+            case "Esfera":
+                return 4 * PI * Math.pow(radio, 2);
+            case "Cilindro":
+                return 2 * PI * radio * (radio + altura);
+            case "Cono":
+                return PI * radio * (radio + generatriz());
+            default:
+                return 0.0;
+        }
     }
 
-    //CILINDRO
-    public Double CalcularAreaCilindro(){
-        Double area = 2 * PI * radio * (radio + altura);
-        return area;
+    public double calcularVolumen(String nombre) {
+        switch (nombre) {
+            case "Cubo":
+                return Math.pow(lado, 3);
+            case "Esfera":
+                return 0.75 * PI * Math.pow(radio, 3);
+            case "Cilindro":
+                return PI * Math.pow(radio, 2) * altura;
+            case "Cono":
+                return 1/3 * PI * Math.pow(radio, 2) * altura;
+            default:
+                return 0.0;
+        }
     }
-    public Double CalcularVolunenCilindro(){
-        Double volumen = PI * Math.pow(radio,2) * altura;
-        return volumen;
-    }
-
-    //CONO
-    public Double CalcularAreaCono(){
-        Double area = PI * radio * (radio + generatriz());
-        return area;
-    }
-    public Double CalcularVolunenCono(){
-        Double volumen = 1/3 * PI * Math.pow(radio,2) * altura;
-        return volumen;
-    }
-
-    public void Calcular() {
-        System.out.println("¿Qué deseas calcular?");
-        System.out.println("1. Area");
-        System.out.println("2. Volumen");
-        System.out.println("3. Retroceder");
-        System.out.println("Seleccina una opcion; ");
-    }
-
-    // ------------------------------------- //
 
     public void Menu(){
         Scanner sc = new Scanner(System.in);
-        int opcion;
-        do {
-            System.out.println("FIGURAS GEOMETRICAS 3D");
-            System.out.println("1. Cubo");
-            System.out.println("2. Esfera");
-            System.out.println("3. Cilindro");
-            System.out.println("4. Cono");
-            System.out.println("Seleccione una opcion: ");
-            opcion = sc.nextInt();
+        int opcion = 0;
+        System.out.println("FIGURAS GEOMETRICAS 3D");
+        System.out.println("1. Cubo");
+        System.out.println("2. Esfera");
+        System.out.println("3. Cilindro");
+        System.out.println("4. Cono");
+        System.out.println("5. Salir");
+        System.out.println("Seleccione una opcion: ");
+        opcion = sc.nextInt();
+        if (opcion == 1) {
+            setNombre("Cubo");
+            System.out.println("AREA");
+            System.out.println("Escribe la longitud de la cara del Cubo: ");
+            setLado(sc.nextDouble());
+            System.out.println("Area Cubo: " + calcularArea("Cubo"));
+            System.out.println("---------------------------------------------");
+            System.out.println("VOLUMEN");
+            System.out.println("Escribe la longitud de la cara del Cubo: ");
+            setLado(sc.nextDouble());
+            System.out.println("Volumen Cubo: " + calcularVolumen("Cubo"));
+            System.out.println("---------------------------------------------");
+        } else if (opcion == 2) {
+            setNombre("Esfera");
+            System.out.println("AREA");
+            System.out.println("Escribe el radio de la Esfera: ");
+            setRadio(sc.nextDouble());
+            System.out.println("Area Esfera: " + calcularArea("Esfera"));
+            System.out.println("---------------------------------------------");
+            System.out.println("VOLUMEN");
+            System.out.println("Escribe el radio de la Esfera: ");
+            setRadio(sc.nextDouble());
+            System.out.println("Volumen Esfera: " + calcularVolumen("Esfera"));
+            System.out.println("---------------------------------------------");
+        } else if (opcion == 3) {
+            setNombre("Cilindro");
+            System.out.println("AREA");
+            System.out.println("Escribe la altura del Cilindro: ");
+            setAltura(sc.nextDouble());
+            System.out.println("Escribe el radio del Cilindro: ");
+            setRadio(sc.nextDouble());
+            System.out.println("Area Cilindro: " + calcularArea("Cilindro"));
+            System.out.println("---------------------------------------------");
+            System.out.println("VOLUMEN");
+            System.out.println("Escribe la altura del Cilindro: ");
+            setAltura(sc.nextDouble());
+            System.out.println("Escribe el radio del Cilindro: ");
+            setRadio(sc.nextDouble());
+            System.out.println("Volumen Cilindro: " + calcularVolumen("Cilindro"));
+            System.out.println("---------------------------------------------");
+        } else if (opcion == 4) {
+            setNombre("Cono");
+            System.out.println("AREA");
+            System.out.println("Escribe la altura del Cono: ");
+            setAltura(sc.nextDouble());
+            System.out.println("Escribe el radio del Cono: ");
+            setRadio(sc.nextDouble());
+            generatriz();
+            System.out.println("Area Cono: " + calcularArea("Cilindro"));
+            System.out.println("---------------------------------------------");
+            System.out.println("VOLUMEN");
+            System.out.println("Escribe la altura del Cono: ");
+            setAltura(sc.nextDouble());
+            System.out.println("Escribe el radio del Cono: ");
+            setRadio(sc.nextDouble());
+            System.out.println("Volumen Cono: " + calcularVolumen("Cilindro"));
+            System.out.println("---------------------------------------------");
+        } else if (opcion == 5) {
+            System.out.println("Saliendo de Figuras 3D");
+        } else {
+            System.out.println("Opción no válida");
+        }
 
-            switch (opcion){
-                case 1:
-                    int opcionFigura;
-                    setNombre("Cubo");
-                    do {
-                        Calcular();
-                        opcionFigura = sc.nextInt();
-                        if (opcionFigura == 1){
-                            System.out.println("Escribe la longitud de la cara del Cubo: ");
-                            setLado(sc.nextDouble());
-                            System.out.println("Area Cubo: " + CalcularAreaCubo());
-                        } else if (opcionFigura == 2){
-                            System.out.println("Escribe la longitud de la cara del Cubo: ");
-                            setLado(sc.nextDouble());
-                            System.out.println("Volumen Cubo: " + CalcularVolumenCubo());
-                        } else if (opcionFigura == 3) {
-                            System.out.println("Volviendo al Menu...");
-                        } else {
-                            System.out.println("Ingresa una opcion correcta");
-                        }
-                    } while (opcionFigura != 3);
-                    break;
-                case 2:
-                    setNombre("Esfera");
-                    int opcionFigura1;
-                    do {
-                        Calcular();
-                        opcionFigura1 = sc.nextInt();
-                        if (opcionFigura1 == 1){
-                            System.out.println("Escribe el radio de la Esfera: ");
-                            setRadio(sc.nextDouble());
-                            System.out.println("Area Esfera: " + CalcularAreaEsfera());
-                        } else if (opcionFigura1 == 2){
-                            System.out.println("Escribe el radio de la Esfera: ");
-                            setRadio(sc.nextDouble());
-                            System.out.println("Volumen Esfera: " + CalcularVolunenEsfera());
-                        } else if (opcionFigura1 == 3) {
-                            System.out.println("Volviendo al Menu...");
-                        } else {
-                            System.out.println("Ingresa una opcion correcta");
-                        }
-                    } while (opcionFigura1 != 3);
-                    break;
-                case 3:
-                    setNombre("Cilindro");
-                    int opcionFigura2;
-                    do {
-                        Calcular();
-                        opcionFigura2 = sc.nextInt();
-                        if (opcionFigura2 == 1){
-                            System.out.println("Escribe la altura del Cilindro: ");
-                            setAltura(sc.nextDouble());
-                            System.out.println("Escribe el radio del Cilindro: ");
-                            setRadio(sc.nextDouble());
-                            System.out.println("Area Cilindro: " + CalcularAreaCilindro());
-                        } else if (opcionFigura2 == 2){
-                            System.out.println("Escribe la altura del Cilindro: ");
-                            setAltura(sc.nextDouble());
-                            System.out.println("Escribe el radio del Cilindro: ");
-                            setRadio(sc.nextDouble());
-                            System.out.println("Volumen Cilindro: " + CalcularVolunenCilindro());
-                        } else if (opcionFigura2 == 3) {
-                            System.out.println("Volviendo al Menu...");
-                        } else {
-                            System.out.println("Ingresa una opcion correcta");
-                        }
-                    } while (opcionFigura2 != 3);
-                    break;
-                case 4:
-                    setNombre("Cono");
-                    int opcionFigura3;
-                    do {
-                        Calcular();
-                        opcionFigura3 = sc.nextInt();
-                        if (opcionFigura3 == 1) {
-                            System.out.println("Escribe la altura del Cono: ");
-                            setAltura(sc.nextDouble());
-                            System.out.println("Escribe el radio del Cono: ");
-                            setRadio(sc.nextDouble());
-                            generatriz();
-                            System.out.println("Area Cubo: " + CalcularAreaCono());
-                        } else if (opcionFigura3 == 2){
-                            System.out.println("Escribe la altura del Cono: ");
-                            setAltura(sc.nextDouble());
-                            System.out.println("Escribe el radio del Cono: ");
-                            setRadio(sc.nextDouble());
-                            System.out.println("Volumen Cubo: " + CalcularVolunenCono());
-                        } else if (opcionFigura3 == 3) {
-                            System.out.println("Volviendo al Menu...");
-                        } else {
-                            System.out.println("Ingresa una opcion correcta");
-                        }
-                    } while (opcionFigura3 != 3);
-                    break;
-                case 5:
-                    System.out.println("Saliendo de Figuras Geometricas 3D");
-                    break;
-                default:
-                    System.out.println("Opcion Invalida");
-            }
-        }while (opcion != 5);
     }
 
+
     @Override
-    public void mostrar_datos() {
+    public void mostrar_datos(){
         System.out.println("Figuras Geometricas 3D");
         System.out.println("Nombre: " + getNombre());
-        System.out.println("Area: " + CalcularAreaCubo());
-        System.out.println("Volumen: " + CalcularVolumenCubo());
+        System.out.println("Area: " + calcularArea(Nombre));
+        System.out.println("Volumen: " + calcularVolumen(Nombre));
     }
 }
